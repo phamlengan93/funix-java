65cffaeccd4df137724bab1a3f34915b8e1db004
nganpl <phamlengan93@gmail.com>
2023-01-06 14:40:04 +0700

    Update code

--- Lab2/Bai1.java
+++ Lab2/Bai1.java
@@ -10,14 +10,16 @@ public class Bai1 {
         boolean dk2 = (N % 4 == 0);
         boolean dk3 = (N % 100 == 0);
 
-        //dk1 la true1
-        if (dk1) {
+        //dk1 la true
+        if ( N % 400 == 0) {
             System.out.println(N + " La nam nhuan");
-        } else if (dk2 == false && dk3 == true) {
-            //dk2 == "false" && dk3 == "true"
+        } else if (N%4==1 && N%100==0)
+        //dk2 == "false" && dk3 == "true"
+            {
             System.out.println(N + " Khong phai nam nhuan");
-        } else if (dk2 == true && dk3 == false) {
-            //dk2 == "true" && dk3 == "false"
+        } else if (N%4==0 && N%100==1)
+        //dk2 == "true" && dk3 == "false"
+            {
             System.out.println(N + " Khong phai nam nhuan");
         } else {
             System.out.println(N + " Khong phai nam nhuan");
